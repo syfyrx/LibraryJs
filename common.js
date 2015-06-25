caffae91a77b3308dc1571ff218048e144a66f2b
syfyrx <522733724@qq.com>
2015-06-25 16:15:32 +0800

    add another js date formate

--- common.js
+++ common.js
@@ -1,68 +1,116 @@
 /* common.js v1.0.0 */
-
+// 对Date的扩展，将 Date 转化为指定格式的String   
+// 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，   
+// 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)   
+// 例子：   
+// (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423   
+// (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
 Date.prototype.format = function (format) {
-	var o = {
-		"M+": this.getMonth() + 1, // month
-		"d+": this.getDate(), // day
-		"h+": this.getHours(), // hour
-		"m+": this.getMinutes(), // minute
-		"s+": this.getSeconds(), // second
-		"q+": Math.floor((this.getMonth() + 3) / 3), // quarter
-		"S": this.getMilliseconds()
-	};
-	if (/(y+)/.test(format))
-		format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-	for (var k in o)
-		if (new RegExp("(" + k + ")").test(format))
-			format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
-	return format;
+    var o = {
+        "M+": this.getMonth() + 1, // month
+        "d+": this.getDate(), // day
+        "h+": this.getHours(), // hour
+        "m+": this.getMinutes(), // minute
+        "s+": this.getSeconds(), // second
+        "q+": Math.floor((this.getMonth() + 3) / 3), // quarter
+        "S": this.getMilliseconds()
+    };
+    if (/(y+)/.test(format))
+        format = format.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    for (var k in o)
+        if (new RegExp("(" + k + ")").test(format))
+            format = format.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+    return format;
 }
-
+/**       
+ * 对Date的扩展，将 Date 转化为指定格式的String       
+ * 月(M)、日(d)、12小时(h)、24小时(H)、分(m)、秒(s)、周(E)、季度(q) 可以用 1-2 个占位符       
+ * 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)       
+ * eg:       
+ * (new Date()).pattern("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423       
+ * (new Date()).pattern("yyyy-MM-dd E HH:mm:ss") ==> 2009-03-10 二 20:09:04       
+ * (new Date()).pattern("yyyy-MM-dd EE hh:mm:ss") ==> 2009-03-10 周二 08:09:04       
+ * (new Date()).pattern("yyyy-MM-dd EEE hh:mm:ss") ==> 2009-03-10 星期二 08:09:04       
+ * (new Date()).pattern("yyyy-M-d h:m:s.S") ==> 2006-7-2 8:9:4.18       
+ */
+Date.prototype.pattern = function (fmt) {
+    var o = {
+        "M+": this.getMonth() + 1, //月份           
+        "d+": this.getDate(), //日           
+        "h+": this.getHours() % 12 == 0 ? 12 : this.getHours() % 12, //小时           
+        "H+": this.getHours(), //小时           
+        "m+": this.getMinutes(), //分           
+        "s+": this.getSeconds(), //秒           
+        "q+": Math.floor((this.getMonth() + 3) / 3), //季度           
+        "S": this.getMilliseconds() //毫秒           
+    };
+    var week = {
+        "0": "/u65e5",
+        "1": "/u4e00",
+        "2": "/u4e8c",
+        "3": "/u4e09",
+        "4": "/u56db",
+        "5": "/u4e94",
+        "6": "/u516d"
+    };
+    if (/(y+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+    }
+    if (/(E+)/.test(fmt)) {
+        fmt = fmt.replace(RegExp.$1, ((RegExp.$1.length > 1) ? (RegExp.$1.length > 2 ? "/u661f/u671f" : "/u5468") : "") + week[this.getDay() + ""]);
+    }
+    for (var k in o) {
+        if (new RegExp("(" + k + ")").test(fmt)) {
+            fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        }
+    }
+    return fmt;
+}
 /* 产生随机颜色 */
 function randomColor() {
-	var rand = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-	if (rand.length == 6) {
-		return rand;
-	} else {
-		return randomColor();
-	}
+    var rand = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    if (rand.length == 6) {
+        return rand;
+    } else {
+        return randomColor();
+    }
 }
 /* 获取url中"?"符后的参数 */
 function getRequest() {
-	var url = location.search; // 获取url中"?"符后的字串
-	var theRequest = new Object();
-	if (url.indexOf("?") != -1) {
-		var str = url.substr(1);
-		strs = str.split("&");
-		for (var i = 0; i < strs.length; i++) {
-			theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
-		}
-	}
-	return theRequest;
+    var url = location.search; // 获取url中"?"符后的字串
+    var theRequest = new Object();
+    if (url.indexOf("?") != -1) {
+        var str = url.substr(1);
+        strs = str.split("&");
+        for (var i = 0; i < strs.length; i++) {
+            theRequest[strs[i].split("=")[0]] = unescape(strs[i].split("=")[1]);
+        }
+    }
+    return theRequest;
 }
 /* 获取屏幕DPI */
 function getScreenDPI() {
-	var arrDPI = new Array();
-	if (window.screen.deviceXDPI != undefined) {
-		arrDPI[0] = window.screen.deviceXDPI;
-		arrDPI[1] = window.screen.deviceYDPI;
-	} else {
-		var tmpNode = document.createElement("DIV");
-		tmpNode.style.cssText = "width:1in;height:1in;position:absolute;left:0px;top:0px;z-index:99;visibility:hidden";
-		document.body.appendChild(tmpNode);
-		arrDPI[0] = parseInt(tmpNode.offsetWidth);
-		arrDPI[1] = parseInt(tmpNode.offsetHeight);
-		tmpNode.parentNode.removeChild(tmpNode);
-	}
-	return arrDPI;
+    var arrDPI = new Array();
+    if (window.screen.deviceXDPI != undefined) {
+        arrDPI[0] = window.screen.deviceXDPI;
+        arrDPI[1] = window.screen.deviceYDPI;
+    } else {
+        var tmpNode = document.createElement("DIV");
+        tmpNode.style.cssText = "width:1in;height:1in;position:absolute;left:0px;top:0px;z-index:99;visibility:hidden";
+        document.body.appendChild(tmpNode);
+        arrDPI[0] = parseInt(tmpNode.offsetWidth);
+        arrDPI[1] = parseInt(tmpNode.offsetHeight);
+        tmpNode.parentNode.removeChild(tmpNode);
+    }
+    return arrDPI;
 }
 /* 获取当前日期，格式：1989-11-24 */
 function getCurrentDate() {
-	var now = new Date();
-	return now.format("yyyy-MM-dd");
+    var now = new Date();
+    return now.format("yyyy-MM-dd");
 }
 /* 获取当前时间，格式：1989-11-24 00:00:00 */
 function getCurrentTime() {
-	var now = new Date();
-	return now.format("yyyy-MM-dd hh:mm:ss");
+    var now = new Date();
+    return now.format("yyyy-MM-dd hh:mm:ss");
 }
