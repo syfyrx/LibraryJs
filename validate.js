dfb5f0fffc832512856f152295f6d940cdf99a03
syfyrx <522733724@qq.com>
2015-10-14 15:12:44 +0800

    bug

--- validate.js
+++ validate.js
@@ -5,3 +5,8 @@ function isPlusInt(txt) {
 	var reg = /^\+?[1-9][0-9]*$/;
 	return reg.test(txt);
 }
+// 验证邮箱
+function isEmail(txt){
+	var reg=/^\w+((-\w+)|(\.\w+))*\@[A-Za-z0-9]+((\.|-)[A-Za-z0-9]+)*\.[A-Za-z0-9]+$/;
+	return reg.test(txt);
+}
